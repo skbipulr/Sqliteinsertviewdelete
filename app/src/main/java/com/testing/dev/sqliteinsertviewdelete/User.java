bd259c28d416bc679f38580dbefb615cb09f5f87
Bipul Sarkar <skbipulr@gmail.com>
2019-12-31 01:36:33 +0600

    Initial commit

---
+++ app/src/main/java/com/testing/dev/sqliteinsertviewdelete/User.java
@@ -0,0 +1,28 @@
+package com.testing.dev.sqliteinsertviewdelete;
+
+public class User {
+    private int id;
+    private String name;
+    private String age;
+
+    public User() {
+    }
+
+    public User(int id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
